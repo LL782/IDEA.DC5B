2930b013d329fa29aec48c5992afa788c8d1181b
ll782 <laurence.lord@gmail.com>
2022-07-21 23:17:20 +0100

    Extract ProductCard

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
 import Image from "next/image";
 import Head from "next/head";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import products from "../data/products";
 import { useBag } from "../bag/useBag";
 import { displayPrice } from "../bag/displayPrice";
 import { PrimaryButton } from "../atomic-ui/PrimaryButton";
+
+const ProductCard = ({ product }) => {
+  const { title, type, description, image, alt, price, subTitle } = product;
+  return (
+    <li key={price.id} className={styles.card}>
+      <h3>{title}</h3>
+      <h4>{subTitle}</h4>
+      <Image
+        src={image}
+        alt={alt}
+        width="800px"
+        height="800px"
+        layout="responsive"
+      />
+      <p className={styles.price}>
+        {displayPrice(price.amount)}{" "}
+        <span className={styles.priceType}>{type}</span>
+      </p>
+      <p className={styles.cardDescription}>{description}</p>
+      <PrimaryButton onClick={() => addToBag({ id: price.id })}>
+        Add to bag
+      </PrimaryButton>
+    </li>
+  );
+};
 
 export default function Home() {
   const { addToBag } = useBag();
@@ -25,36 +49,9 @@ export default function Home() {
         </p>
 
         <ul className={styles.grid}>
-          {products.map((product) => {
-            const { title, type, description, image, alt, price, subTitle } =
-              product;
-
-            return (
-              <li key={price.id} className={styles.card}>
-                {/* <Link href={`/products/${price.id}`}> */}
-                {/* <a> */}
-                <h3>{title}</h3>
-                <h4>{subTitle}</h4>
-                <Image
-                  src={image}
-                  alt={alt}
-                  width="800px"
-                  height="800px"
-                  layout="responsive"
-                />
-                <p className={styles.price}>
-                  {displayPrice(price.amount)}{" "}
-                  <span className={styles.priceType}>{type}</span>
-                </p>
-                <p className={styles.cardDescription}>{description}</p>
-                {/* </a> */}
-                {/* </Link> */}
-                <PrimaryButton onClick={() => addToBag({ id: price.id })}>
-                  Add to bag
-                </PrimaryButton>
-              </li>
-            );
-          })}
+          {products.map((product) => (
+            <ProductCard product={product} />
+          ))}
         </ul>
       </main>
     </div>
