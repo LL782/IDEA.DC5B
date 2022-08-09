60e61a0ca4ec73611831d7f207d91018900620dc
ll782 <laurence.lord@gmail.com>
2022-08-09 23:07:21 +0100

    Reposition the extra cards

--- optionalExtraCard/OptionalExtraCard.tsx
+++ optionalExtraCard/OptionalExtraCard.tsx
@@ -1,7 +1,7 @@
 import { Prototypes, YourIdeas } from "./extraCards";
 
 export const OptionalExtraCard = ({ mapIndex }) => {
-  if (mapIndex === 0) return <Prototypes />;
-  if (mapIndex === 2) return <YourIdeas />;
+  if (mapIndex === 3) return <Prototypes />;
+  if (mapIndex === 6) return <YourIdeas />;
   return null;
 };
--- optionalExtraCard/extraCards.tsx
+++ optionalExtraCard/extraCards.tsx
@@ -5,7 +5,7 @@ export const YourIdeas = () => (
     <h2>Your ideas:</h2>
     <p className={styles.cardDescription}>
       What else can we do? What can we do instead? Let us know in as much detail
-      as you want and we'll look into it for you.
+      as you want and we&apos;ll look into it for you.
     </p>
     <p className={styles.cardDescription}>
       Dreaming. We can manifest new artwork and other beautiful things –
@@ -18,13 +18,13 @@ export const Prototypes = () => (
   <li className={styles.card}>
     <h2>Prototypes</h2>
     <p className={styles.cardDescription}>
-      This is all new to us (to me really). We're putting ideas out here to get
-      feedback. What do you like? What works? What doesn't?
+      This is all new to us (to me really). We&apos;re putting ideas out here to
+      get feedback. What do you like? What works? What doesn&apos;t?
     </p>
     <p className={styles.cardDescription}>
-      Your feedback is not only welcome, it's the way this business is going to
-      grow. Within reason, we want to provide artwork in exactly the way you
-      want it.
+      Your feedback is not only welcome, it&apos;s the way this business is
+      going to grow. Within reason, we want to provide artwork in exactly the
+      way you want it.
     </p>
     <p className={styles.cardDescription}>
       T-shirt, coaster, wall vinyl, colouring in book, huge oil paintings, brass
