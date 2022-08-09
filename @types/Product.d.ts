3874e02990a9a62846709999dc7c41a7aae3c5bc
ll782 <laurence.lord@gmail.com>
2022-08-09 21:03:14 +0100

    Draft a type definition for Products

---
+++ @types/Product.d.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  alt: string;
+  description: string;
+  image: string;
+  price: {
+    amount: number;
+    id: string;
+  };
+  title: string;
+  subTitle: string;
+  type: string;
+};
--- data/products.ts
+++ data/products.ts
@@ -1,6 +1,7 @@
+import { Product } from "../@types/Product";
 import dev from "./products.dev";
 import prod from "./products.prod";
 
-const products = process.env.NODE_ENV === "production" ? prod : dev;
+const products: Product[] = process.env.NODE_ENV === "production" ? prod : dev;
 
 export default products;
