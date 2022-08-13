732b6e3380dd938b5bf696ebed0d053e454441ca
ll782 <laurence.lord@gmail.com>
2022-08-13 20:10:00 +0100

    Extract AddToBag

--- atomic-ui/ProductCard.tsx
+++ atomic-ui/ProductCard.tsx
@@ -6,9 +6,18 @@ import { PrimaryButton } from "./PrimaryButton";
 import { useBag } from "../bag/useBag";
 import { PriceTag } from "./PriceTag";
 
+const AddToBag = ({ price }) => {
+  const { addToBag } = useBag();
+
+  const handleAdd = () => {
+    addToBag({ id: price.id });
+  };
+
+  return <PrimaryButton onClick={handleAdd}>Add to bag</PrimaryButton>;
+};
+
 export const ProductCard = ({ product }) => {
   const { title, type, description, image, alt, price, subTitle } = product;
-  const { addToBag } = useBag();
 
   return (
     <li className={styles.card}>
@@ -27,9 +36,7 @@ export const ProductCard = ({ product }) => {
       </Link>
       <PriceTag price={price} type={type} />
       <p className={styles.cardDescription}>{description}</p>
-      <PrimaryButton onClick={() => addToBag({ id: price.id })}>
-        Add to bag
-      </PrimaryButton>
+      <AddToBag price={price} />
     </li>
   );
 };
