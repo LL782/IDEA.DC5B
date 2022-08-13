30818712d30397a5cd9a09f2cf33b3ebe95bf9dc
ll782 <laurence.lord@gmail.com>
2022-08-13 20:24:38 +0100

    Avoid adding any price more than once

--- atomic-ui/AddToBag.tsx
+++ atomic-ui/AddToBag.tsx
@@ -2,11 +2,14 @@ import { PrimaryButton } from "./PrimaryButton";
 import { useBag } from "../bag/useBag";
 
 export const AddToBag = ({ price }) => {
-  const { addToBag } = useBag();
+  const { addToBag, bagItems, updateItem } = useBag();
+  const inBag = bagItems.some(({ id }) => id === price.id);
+  const text = inBag ? "In the bag" : "Add to bag";
 
-  const handleAdd = () => {
-    addToBag({ id: price.id });
-  };
+  const handleAdd = () =>
+    inBag
+      ? updateItem({ id: price.id, quantity: 1 })
+      : addToBag({ id: price.id });
 
-  return <PrimaryButton onClick={handleAdd}>Add to bag</PrimaryButton>;
+  return <PrimaryButton onClick={handleAdd}>{text}</PrimaryButton>;
 };
