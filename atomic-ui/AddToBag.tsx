72e552c4592c2871ac5770a9d651ed008dbf049e
ll782 <laurence.lord@gmail.com>
2022-08-13 23:56:08 +0100

    Go to town on AddToBag text

--- atomic-ui/AddToBag.tsx
+++ atomic-ui/AddToBag.tsx
@@ -1,19 +1,35 @@
+import { BagItem } from "../@types/Product";
 import { PrimaryButton } from "./PrimaryButton";
 import { useBag } from "../bag/useBag";
+
+const NUMBER_WORD = { 2: "Two", 3: "Three", 4: "Four", 5: "Five" };
+
+interface ButtonTextProps {
+  bagItem: BagItem;
+  maxedOut: boolean;
+}
+
+const ButtonText = ({ bagItem, maxedOut }: ButtonTextProps) => {
+  if (!bagItem) return <>Add to bag</>;
+  if (bagItem.quantity === 1 && maxedOut) return <>Last one in the bag 😃</>;
+  if (bagItem.quantity === 1) return <>In the bag</>;
+  if (maxedOut) return <>Maxed out 🎉</>;
+
+  return (
+    <>{`${NUMBER_WORD[bagItem.quantity] || bagItem.quantity} in the bag`}</>
+  );
+};
 
 export const AddToBag = ({ maxQuantity = 5, price }) => {
   const { addToBag, bagItems } = useBag();
   const bagItem = bagItems.filter(({ id }) => id === price.id)[0];
-  const inBag = !!bagItem;
   const maxedOut = bagItem?.quantity >= maxQuantity;
 
   const handleAdd = () => addToBag({ id: price.id });
 
-  const text = maxedOut ? "Maxed out" : inBag ? "In the bag" : "Add to bag";
-
   return (
     <PrimaryButton disabled={maxedOut} onClick={handleAdd}>
-      {text}
+      <ButtonText bagItem={bagItem} maxedOut={maxedOut} />
     </PrimaryButton>
   );
 };
--- data/ideas.base.ts
+++ data/ideas.base.ts
@@ -28,7 +28,7 @@ export const spaceAgeMug: BaseIdea = {
   description:
     "This is chinagraph pencil on porceline so don't smudge it. Recommended for a careful collector. If people like it I'll do a print run of non-unique, cheaper ones. Let me know",
   image: "/product-images/spaceAgeMug.jpg",
-  maxQuantity: 4,
+  maxQuantity: 1,
   subTitle: "Test artwork on a big mug",
   title: "Space Age Mug",
   type: "Original prototype",
