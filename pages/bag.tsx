a98b864a30e0a1e4567b5b9cc32e8462f8032e00
ll782 <laurence.lord@gmail.com>
2022-08-14 00:22:42 +0100

    Add title to Bag page

--- pages/bag.tsx
+++ pages/bag.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
 import { Bag } from "../bag/Bag";
 
-export default Bag;
+export default function BagPage() {
+  return (
+    <>
+      <Head>
+        <title>S H O P . D C 5 B / B A G </title>
+      </Head>
+      <Bag />
+    </>
+  );
+}
