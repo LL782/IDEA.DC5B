bd007a47e3714f6e203e9dc851f81a0417a5fd49
ll782 <laurence.lord@gmail.com>
2022-08-19 01:01:02 +0100

    Add details to 004

--- ideaDetails/customSections/index.tsx
+++ ideaDetails/customSections/index.tsx
@@ -6,6 +6,7 @@ const customContent = {
   "bearded-strange-face-colouring": dynamic(
     () => import("./bearded-strange-face-colouring")
   ),
+  "rodwell-hat-trick-004": dynamic(() => import("./rodwell-hat-trick-004")),
 };
 
 export const CustomSections = ({ id }) => {
---
+++ ideaDetails/customSections/rodwell-hat-trick-004.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function CustomContent() {
+  return (
+    <>
+      <p>In the picture below #4 is on top of the framed pile on the right.</p>
+      <Image
+        src={
+          "/idea-images/rodwell-hat-trick-004/Touchdown-Rodwell_004_Framed.jpg"
+        }
+        alt={"Black and white version of the main artwork"}
+        width="4032px"
+        height="3024px"
+        layout="responsive"
+      />{" "}
+      <p>
+        Also frame are #6-10. In the middle, finished and unframed, are #11-25.
+        To their left are #26-50. When this photo was taken they had just
+        finished getting their first ink lines and still have graphite guides.
+        Lastly, you have the blanks for #51-250 stacked on the left.
+      </p>
+    </>
+  );
+}
