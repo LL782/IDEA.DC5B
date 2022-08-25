ba6bb3dad2399af4102a251b41c50c9cff3c68ab
ll782 <laurence.lord@gmail.com>
2022-08-25 01:16:41 +0100

    Ensure we push local data for all actions

--- noteWebActions/noteWebActions.test.ts
+++ noteWebActions/noteWebActions.test.ts
@@ -3,17 +3,27 @@ import { noteWebActions } from "./noteWebActions";
 
 describe("noteWebActions", () => {
   describe("Given a browser that supports localstorage", () => {
-    describe("When triggered with a button click", () => {
+    describe("When triggered with a page view", () => {
       beforeEach(() => {
-        noteWebActions({ action: "click button" });
+        noteWebActions({ action: "viewPage" });
       });
-      it('notes the "click button" action', () => {
+      it('notes the "page view" action', () => {
         expect(
           JSON.parse(
             window.localStorage.getItem("SHOP_DC5B_INTERACTIONS") || ""
           )
-        ).toEqual([{ action: "click button" }]);
+        ).toEqual([{ action: "viewPage" }]);
       });
+    });
+  });
+  describe("and then a button click", () => {
+    beforeEach(() => {
+      noteWebActions({ action: "clickButton" });
+    });
+    it('notes the "click button" action following the page view', () => {
+      expect(
+        JSON.parse(window.localStorage.getItem("SHOP_DC5B_INTERACTIONS") || "")
+      ).toEqual([{ action: "viewPage" }, { action: "clickButton" }]);
     });
   });
 });
--- noteWebActions/noteWebActions.ts
+++ noteWebActions/noteWebActions.ts
@@ -1,6 +1,19 @@
-export function noteWebActions(arg0: { action: string }) {
+const LOCAL_STORAGE_KEY = "SHOP_DC5B_INTERACTIONS";
+
+type WebAction = { action: string };
+type WebActionsData = Array<WebAction>;
+
+const initialState = "[]";
+
+export function noteWebActions(input: { action: string }) {
+  const localData: WebActionsData = JSON.parse(
+    window.localStorage.getItem(LOCAL_STORAGE_KEY) || initialState
+  );
+
+  localData.push({ action: input.action });
+
   window.localStorage.setItem(
     "SHOP_DC5B_INTERACTIONS",
-    JSON.stringify([{ action: "click button" }])
+    JSON.stringify(localData)
   );
 }
