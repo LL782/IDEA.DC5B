b1012c0a5b46837709b70cf8a7f0522ddb885eb5
Laurence Lord <laurence.lord@gmail.com>
2022-09-12 22:35:45 +0100

    Prefer a real product for test

--- src/productIdeas/_example/index.tsx
+++ src/productIdeas/_example/index.tsx
@@ -1,18 +1,7 @@
 import { PricedProduct } from "../businessLogic/Idea";
+import { products } from "../data/ideas";
 import { ProductDetails } from "../ui/ProductDetails";
 
-export const example: PricedProduct = {
-  alt: "testAlt",
-  description: "testDescription",
-  id: "testId",
-  image: "/testImage",
-  price: {
-    amount: 999,
-    id: "testPriceId",
-  },
-  subTitle: "testSubTitle",
-  title: "testTitle",
-  type: "Test Idea Type",
-};
+export const example: PricedProduct = products[0];
 
 export const ExampleProductDetails = () => <ProductDetails idea={example} />;
