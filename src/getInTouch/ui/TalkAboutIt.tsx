3161d8750404a156c4a6d3f3107ee3bd7e706a40
ll782 <laurence.lord@gmail.com>
2022-09-13 21:27:48 +0100

    Create primary type of TalkAboutIt

--- src/getInTouch/ui/TalkAboutIt.tsx
+++ src/getInTouch/ui/TalkAboutIt.tsx
@@ -1,16 +1,23 @@
+import { PrimaryLink } from "src/atomic-ui/PrimaryLink";
 import { SecondaryLink } from "../../atomic-ui/SecondaryLink";
 
 interface Props {
   blogPost?: string;
+  type?: "primary";
 }
 
-export const TalkAboutIt = ({ blogPost }: Props) => {
+export const TalkAboutIt = ({ blogPost, type }: Props) => {
   if (!blogPost) {
     return null;
   }
+  const text = "Let's talk about it...";
   return (
     <p>
-      <SecondaryLink href={blogPost}>Let&apos;s talk about it...</SecondaryLink>
+      {type === "primary" ? (
+        <PrimaryLink href={blogPost}>{text}</PrimaryLink>
+      ) : (
+        <SecondaryLink href={blogPost}>{text}</SecondaryLink>
+      )}
     </p>
   );
 };
