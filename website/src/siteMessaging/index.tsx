009568bc68c01285d0caee5b94b38e5789add7ac
Laurence Lord <laurence.lord@gmail.com>
2022-11-13 18:50:28 +0000

    Add placeholder for idea card decoration

--- website/src/siteMessaging/index.tsx
+++ website/src/siteMessaging/index.tsx
@@ -1,6 +1,8 @@
 import styles from "../atomic-ui/IdeaCard.module.css";
 import Link from "next/link";
 import { noteButtonClick } from "src/webActions/noteButtonClick";
+import Image from "next/image";
+import { cardImageSizes } from "src/atomic-ui/cardImageSizes";
 
 interface Props {
   mapIndex: number;
@@ -13,49 +15,70 @@ export const OptionalExtraCard = ({ mapIndex }: Props) => {
 };
 
 const SiteIsYoung = () => (
-  <li className={styles.card}>
-    <h2>The site is young</h2>
-    <p className={styles.cardDescription}>
-      DC5B is a brand under development. It&apos;s not only the products that
-      you can influence at this stage, it’s the whole business! Add your ideas
-      into the mix by telling us what you think.
-    </p>
-    <p className={styles.cardDescription}>
-      <Link
-        onClick={() => noteButtonClick("email", "The site is young")}
-        href="mailto:dc5b@dc5b.com?subject=Where%20shall%20we%20go%20from%20here%3F&body=I've%20just%20been%20on%20your%20website%20and%20wanted%20to%20get%20in%20touch%20with%20an%20idea%20of%20my%20own...%0D%0A%0D%0A%3A)%0D%0A%0D%0A"
-        target="_blank"
-        className={styles.littleLink}
-      >
-        Where shall we go from here?
-      </Link>
-    </p>
+  <li className={`${styles.card} ${styles.card__decorated}`}>
+    <div className={styles.content}>
+      <h2>The site is young</h2>
+      <p className={styles.cardDescription}>
+        DC5B is a brand under development. It&apos;s not only the products that
+        you can influence at this stage, it’s the whole business! Add your ideas
+        into the mix by telling us what you think.
+      </p>
+      <p className={styles.cardDescription}>
+        <Link
+          onClick={() => noteButtonClick("email", "The site is young")}
+          href="mailto:dc5b@dc5b.com?subject=Where%20shall%20we%20go%20from%20here%3F&body=I've%20just%20been%20on%20your%20website%20and%20wanted%20to%20get%20in%20touch%20with%20an%20idea%20of%20my%20own...%0D%0A%0D%0A%3A)%0D%0A%0D%0A"
+          target="_blank"
+          className={styles.littleLink}
+        >
+          Where shall we go from here?
+        </Link>
+      </p>
+    </div>
+    <div className={styles.decoration}>
+      {/* <Image
+        src={"/idea-images/bearded-strange-face-colouring.jpg"}
+        alt={""}
+        fill
+        sizes={cardImageSizes}
+      /> */}
+    </div>
   </li>
 );
 
 const Customisation = () => (
-  <li className={styles.card}>
-    <h2>Customisation</h2>
-    <p className={styles.cardDescription}>
-      Thank you for checking out the ideas, your thoughts are invaluable —
-      meaning vital. If you see something you like (or don’t like) let us know.
-    </p>
-    <p className={styles.cardDescription}>
-      We develop ideas based on feedback. Whether it’s size, format, price,
-      colour, quality or something else everybody wants it slightly different.
-      Just let us know.
-    </p>
-    <p className={styles.cardDescription}>
-      <Link
-        onClick={() => noteButtonClick("email", "Customisation")}
-        href={
-          "mailto:dc5b@dc5b.com?subject=Customisation&body=Hi%20there%2C%0D%0A%0D%0AI've%20just%20been%20on%20your%20website%20and%20have%20some%20ideas...%0D%0A%0D%0A-%20Do%20you%20do%20...%20%3F%0D%0A-%20I%20like%20...%20but%20not%20the%20...%0D%0A-%20Would%20you%20work%20on%20...%20%3F%0D%0A%0D%0AThanks%20very%20much!"
-        }
-        target="_blank"
-        className={styles.littleLink}
-      >
-        Send a message
-      </Link>
-    </p>
+  <li className={`${styles.card} ${styles.card__decorated}`}>
+    <div className={styles.content}>
+      <h2>Customisation</h2>
+      <p className={styles.cardDescription}>
+        Thank you for checking out the ideas, your thoughts are invaluable —
+        meaning vital. If you see something you like (or don’t like) let us
+        know.
+      </p>
+      <p className={styles.cardDescription}>
+        We develop ideas based on feedback. Whether it’s size, format, price,
+        colour, quality or something else everybody wants it slightly different.
+        Just let us know.
+      </p>
+      <p className={styles.cardDescription}>
+        <Link
+          onClick={() => noteButtonClick("email", "Customisation")}
+          href={
+            "mailto:dc5b@dc5b.com?subject=Customisation&body=Hi%20there%2C%0D%0A%0D%0AI've%20just%20been%20on%20your%20website%20and%20have%20some%20ideas...%0D%0A%0D%0A-%20Do%20you%20do%20...%20%3F%0D%0A-%20I%20like%20...%20but%20not%20the%20...%0D%0A-%20Would%20you%20work%20on%20...%20%3F%0D%0A%0D%0AThanks%20very%20much!"
+          }
+          target="_blank"
+          className={styles.littleLink}
+        >
+          Send a message
+        </Link>
+      </p>
+    </div>
+    <div className={styles.decoration}>
+      {/* <Image
+        src={"/idea-images/bearded-strange-face-colouring.jpg"}
+        alt={""}
+        fill
+        sizes={cardImageSizes}
+      /> */}
+    </div>
   </li>
 );
