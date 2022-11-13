98527848f689afdef8aed622235f10d61e55b0d5
Laurence Lord <laurence.lord@gmail.com>
2022-11-13 17:42:58 +0000

    Display poster as standalone page

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,24 +1,39 @@
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
-
+import { useRouter } from "next/router";
 import SEO from "../../next-seo.config";
 import { BagToolkitProvider } from "../shoppingBag/BagToolkit";
 import { Navigation } from "../siteLayout/ui/Navigation";
 import Footer from "../siteLayout/ui/Footer";
 import { NotePageView } from "src/webActions/NotePageView";
-
 import "../siteLayout/ui/globals.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <DefaultSeo {...SEO} />
-    <BagToolkitProvider>
-      <NotePageView component={Component} />
-      <Navigation />
-      <Component {...pageProps} />
-      <Footer />
-    </BagToolkitProvider>
-  </>
-);
+const standAlonePages = ["/poster"];
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { route } = useRouter();
+  const isStandAlonePage = standAlonePages.includes(route);
+
+  if (isStandAlonePage) {
+    return (
+      <>
+        <NotePageView component={Component} />
+        <Component />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <BagToolkitProvider>
+        <NotePageView component={Component} />
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </BagToolkitProvider>
+    </>
+  );
+};
 
 export default MyApp;
