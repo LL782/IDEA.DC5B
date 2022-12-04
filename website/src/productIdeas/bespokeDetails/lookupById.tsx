ee42ccf13eebf6d61c2334c270b6ce9c2e45fd35
Laurence Lord <laurence.lord@gmail.com>
2022-12-04 00:15:46 +0000

    Remove bespoke details from Bearded Strangeface

--- website/src/productIdeas/bespokeDetails/lookupById.tsx
+++ website/src/productIdeas/bespokeDetails/lookupById.tsx
@@ -3,9 +3,6 @@ import { ComponentType } from "react";
 
 export const lookupById: { [key: string]: ComponentType } = {
   "balsamic-potion": dynamic(() => import("./content/balsamic-potion")),
-  "bearded-strange-face-colouring": dynamic(
-    () => import("./content/bearded-strange-face-colouring")
-  ),
   "rodwell-hat-trick-004": dynamic(
     () => import("./content/rodwell-hat-trick-004")
   ),
