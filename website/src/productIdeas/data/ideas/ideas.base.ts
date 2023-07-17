54978949b4b46a0214e358f70b4b53d6f7b57b22
Laurence "DC5B" Lord <laurence.lord@gmail.com>
2023-07-17 11:15:34 +0100

    Update blog post links

--- website/src/productIdeas/data/ideas/ideas.base.ts
+++ website/src/productIdeas/data/ideas/ideas.base.ts
@@ -7,13 +7,13 @@ export const fastMoving: RawIdea = {
   type: "Raw idea",
   description:
     "This sketch is the first raw idea we've posted. It's here so we can get feedback on it. We'll post more raw ideas in time and rework the popular ones.",
-  blogPost: "https://dc5b.dog/2022/09/10/fast-moving-consumer-gods/",
+  blogPost: "https://dc5b.wordpress.com/2022/09/10/fast-moving-consumer-gods/",
   id: "fast-moving",
 };
 
 export const rodwellHatTrick007: Idea = {
   alt: "",
-  blogPost: "https://dc5b.dog/2022/08/19/so-you-want-to-talk-about-rodwell/",
+  blogPost: "https://dc5b.wordpress.com/2022/08/19/so-you-want-to-talk-about-rodwell/",
   description:
     "Head gone, dragon calm, bush distributed, green detached. Piece seven of Touchdown Rodwell. Sixty square centimetres of Fabriano 5, Copic coloured, Rotring inked, expertly framed.",
   id: "rodwell-hat-trick-007",
@@ -25,7 +25,7 @@ export const rodwellHatTrick007: Idea = {
 
 export const rodwellHatTrick006: Idea = {
   alt: "The dragon in this Touchdown Rodwell has reached the right hand edge of the frame and dips in towards some greenery. The red berries in the bushes have moved and are breaking out white in places. The fire growls low.",
-  blogPost: "https://dc5b.dog/2022/08/19/so-you-want-to-talk-about-rodwell/",
+  blogPost: "https://dc5b.wordpress.com/2022/08/19/so-you-want-to-talk-about-rodwell/",
   description:
     "In number six the dragon takes a dive for the green, the fire grows low and the berries turn white, ready to burst. One of a series of five-hundred hand-drawn multiples – ink on paper.",
   id: "rodwell-hat-trick-006",
@@ -37,7 +37,7 @@ export const rodwellHatTrick006: Idea = {
 
 export const rodwellHatTrick005: Idea = {
   alt: "Another Touchdown Rodwell image. It features a long pink, red and blue dragon, white,yellow and orange clouds, bright blue sky, a yellow field and green bushes.",
-  blogPost: "https://dc5b.dog/2022/08/19/so-you-want-to-talk-about-rodwell/",
+  blogPost: "https://dc5b.wordpress.com/2022/08/19/so-you-want-to-talk-about-rodwell/",
   description:
     "Number five of five-hundred. A hand-drawn multiple from our very own Dog 5b. Drawn in London in 2011 using Copic colours, Rotring ink and Fabriano 5 paper. Framed by E17 Arthouse.",
   id: "rodwell-hat-trick-005",
@@ -50,7 +50,7 @@ export const rodwellHatTrick005: Idea = {
 
 export const rodwellHatTrick004: Idea = {
   alt: "Clean image of bold and colourful artwork on white paper. Thin black lines work with distorted geometric blocks to describe a semi abstract, surreal landscape.",
-  blogPost: "https://dc5b.dog/2022/08/19/so-you-want-to-talk-about-rodwell/",
+  blogPost: "https://dc5b.wordpress.com/2022/08/19/so-you-want-to-talk-about-rodwell/",
   description:
     "Begun in 2009, Touchdown Rodwell is a series of five hundred hand-drawn multiples in Copic and Rotring inks on Fabriano 5 paper. This piece, from 2011, is the fourth of the series.",
   id: "rodwell-hat-trick-004",
@@ -63,7 +63,7 @@ export const rodwellHatTrick004: Idea = {
 export const niceBitOfWrapping: Idea = {
   alt: "Photograph of a little box nicely wrapped up in glossy, black and white paper. There's a plant and a heavy kitchen mortar in the composition too, all on top of a wooden desk",
   blogPost:
-    "https://dc5b.dog/2022/08/16/about-this-nice-bit-of-wrapping-paper-then/",
+    "https://dc5b.wordpress.com/2022/08/16/about-this-nice-bit-of-wrapping-paper-then/",
   description:
     "A3 wrapping paper prototype. Stiffer and heavier than usual, it is pleasure to fold and has a lovely matt finish. Checkout the blog post for more images and details.",
   id: "nice-bit-of-wrapping",
@@ -75,7 +75,7 @@ export const niceBitOfWrapping: Idea = {
 
 export const balsamicPotion: Idea = {
   alt: "Crisp, clean photograph of a hand decorated bottle of Balsamic Vinegar",
-  blogPost: "https://dc5b.dog/2022/08/16/the-balsamo-discussion/",
+  blogPost: "https://dc5b.wordpress.com/2022/08/16/the-balsamo-discussion/",
   description:
     "Bit of a weird one. Do you like this bottle? It looks a bit like a magic potion. In actual fact it's balsamic vinegar, timelessly decorated in glass, masking tape and rubber.",
   id: "balsamic-potion",
@@ -88,7 +88,7 @@ export const balsamicPotion: Idea = {
 export const spaceAgeMug: Idea = {
   alt: "A white porceline mug that I've drawn on with a black chinagraph pencil",
   blogPost:
-    "https://dc5b.dog/2022/08/16/why-you-should-and-you-shouldnt-buy-this-mug/",
+    "https://dc5b.wordpress.com/2022/08/16/why-you-should-and-you-shouldnt-buy-this-mug/",
   description:
     "This is chinagraph pencil on porceline so don't smudge it. Recommended for a careful collector. Would you like it at a different price and without the smudging, let us know.",
   id: "space-age-mug",
@@ -101,7 +101,7 @@ export const spaceAgeMug: Idea = {
 export const losAtlantisColouring: Idea = {
   alt: "Black and white artists impression of a Toltec warrior standing in from of Los Atlantis statues with a flying saucer hovering above.",
   blogPost:
-    "https://dc5b.dog/2022/08/16/los-atlantis-colouring-paper-is-a-pretty-safe-bet/",
+    "https://dc5b.wordpress.com/2022/08/16/los-atlantis-colouring-paper-is-a-pretty-safe-bet/",
   description:
     "In Tula you can see them. Very strange! In 2008 Dog was asked to draw them with a Toltec Warrior and UFO. Now you can download that scene and print it till your heart's content.",
   id: "los-atlantis-colouring",
@@ -113,7 +113,7 @@ export const losAtlantisColouring: Idea = {
 export const eyesOnTheBlindfoldColouring: Idea = {
   alt: "Ink drawing of an unusual face with a blindfold covering the eyes, with eyes printed on the blindfold.",
   blogPost:
-    "https://dc5b.dog/2022/08/16/can-i-tell-you-anything-about-eyes-on-the-blindfold-colouring-in-can-you-tell-me-anything/",
+    "https://dc5b.wordpress.com/2022/08/16/can-i-tell-you-anything-about-eyes-on-the-blindfold-colouring-in-can-you-tell-me-anything/",
   description:
     "A colouring sheet. Two faces. One on the front and one on the back. One face has been blindfolded. The other has the eyes closed. The only open eyes are on the blindfold.",
   id: "eyes-on-the-blindfold-colouring",
@@ -124,7 +124,7 @@ export const eyesOnTheBlindfoldColouring: Idea = {
 
 export const beardedStrangeFaceColouring: Idea = {
   alt: "A coloured in drawing of a strange bearded face.",
-  blogPost: "https://dc5b.dog/2022/08/16/lets-talk-about-bearded-strangeface/",
+  blogPost: "https://dc5b.wordpress.com/2022/08/16/lets-talk-about-bearded-strangeface/",
   description:
     "This bearded face, of ambigious adornment, is shown here in colour. It's been redrawn in black and white at a bigger size to give you a colouring in version, ready to download and print.",
   id: "bearded-strange-face-colouring",
